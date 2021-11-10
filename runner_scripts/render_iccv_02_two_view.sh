f7a99cc3ef0f9e6bd4b723ee0e37d9d307ef5dbd
Guowei <frankhome61@hotmail.com>
2021-11-09 20:35:12 -0800

    added render script

---
+++ runner_scripts/render_iccv_02_two_view.sh
@@ -0,0 +1,2 @@
+pip install --upgrade imageio-ffmpeg
+python3 free_viewpoint_rendering.py --input /sharedvol1/users/frank/nonrigid-nerf/experiments/iccv-02-two-view/ --deformations all --camera_path spiral
